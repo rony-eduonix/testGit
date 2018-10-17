51864f2256a10d8bbca1539d9b737e0adb79285d
Rony <rony.pardhe@gmail.com>
2018-10-17 16:36:02 +0530

    aaded looooo

--- app/src/main/java/rony/jmfinancial/com/myapplication/MainActivity.java
+++ app/src/main/java/rony/jmfinancial/com/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Log.e("","testing tariti dgsdfsdf   ");
+        Log.e("","df   ");
     }
 }
